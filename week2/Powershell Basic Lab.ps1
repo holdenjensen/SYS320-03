550433ac4426b90c5cd897e11ab2dfc0137560f8
holdenjensen <holden.jensen@mymail.champlain.edu>
2024-09-05 15:31:01 -0400

    Week 2 script

---
+++ week2/Powershell Basic Lab.ps1
@@ -0,0 +1,3 @@
+﻿$files = Get-ChildItem -Recurse -File | Where-Object { $_.Extension -eq '.csv' }
+$files | Rename-Item -NewName { $_.Name -replace '.csv', '.log' }
+Get-ChildItem -Recurse -File | Select-Object Name, LastWriteTime, Length
