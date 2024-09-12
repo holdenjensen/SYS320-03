a2fbe7001ae5af6fe0a6ae828f82139f32531fbe
holdenjensen <holden.jensen@mymail.champlain.edu>
2024-09-12 15:23:17 -0400

    Delete week2/Question6.ps1

--- week2/Question6.ps1
+++
@@ -1,9 +0,0 @@
-﻿. (Join-Path $PSScriptRoot 'Questions1-5.ps1')
-
-clear
-
-$loginoutsTable = Get-LoginLogoffEvents -days 15
-$loginoutsTable
-
-$shutdownsTable = Get-StartupShutdownEvents -days 25
-$shutdownsTable
