f7a7f337fe990a326eb87772d9d1f2c6c24d2596
holdenjensen <holden.jensen@mymail.champlain.edu>
2024-10-03 16:05:05 -0400

    Work of 10/4

--- week5/ClassScraper.ps1
+++ week5/ClassScraper.ps1
@@ -44,3 +44,24 @@ function daysTranslator($FullTable){
 
 #Deliverable 1 
 $FullTable | Select-Object "Class Code", Instructor, Location, Days, "Time Start", "Time End" | Where {$_."Instructor" -ilike "* Paligu" }
+
+#Deliverable 2
+$FullTable | Where-Object { ($_.Location -eq "JOYC 310") -and ($_.Days -contains "Monday") } | Sort-Object "Time Start" | Select-Object "Time Start", "Time End", "Class Code"
+
+#Deliverable 3
+$ITSInstructors = $FullTable | Where-Object { 
+    ($_. "Class Code" -like "SYS*") -or
+    ($_. "Class Code" -like "NET*") -or
+    ($_. "Class Code" -like "SEC*") -or
+    ($_. "Class Code" -like "FOR*") -or
+    ($_. "Class Code" -like "CSI*") -or
+    ($_. "Class Code" -like "DAT*")
+} | Select-Object "Instructor" -Unique | Sort-Object "Instructor"
+
+$ITSInstructors
+
+#Deliverable 4
+$FullTable | Where-Object { $_.Instructor -in $ITSInstructors.Instructor } |
+    Group-Object -Property "Instructor" | 
+    Select-Object Count, Name | 
+    Sort-Object Count -Descending
